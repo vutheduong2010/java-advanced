7d59e84816b500acac204b0e62873db155737163
vutheduong2010 <160297939+vutheduong2010@users.noreply.github.com>
2024-11-09 18:57:39 +0700

    create

--- src/main/java/entity/Department.java
+++ src/main/java/entity/Department.java
@@ -4,6 +4,10 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -18,4 +22,22 @@ public class Department {
 
     @Column(name = "name", length = 50, unique = true, nullable = false)
     private String name;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersit() {
+        System.out.println("Trước khi thêm vào database");
+    }
+
+    @PostPersist
+    public void postPersist() {
+        System.out.println("Sau khi thêm vào database");
+    }
 }
